a17bcce43b743375c4da6246d3fa6426bbe6d66d
Adrian Tache <contact@adriantache.com>
2018-04-26 09:20:05 +0300

    Add to-dos

--- app/src/main/java/com/adriantache/manasia_events/MainActivity.java
+++ app/src/main/java/com/adriantache/manasia_events/MainActivity.java
@@ -10,4 +10,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //todo create custom list layout
+
+    //todo create event class and event info subclass
+
+    //todo create custom adapter to display custom list layout
+
+    //todo add onclick and toggle logic for buttons and logo
+
+    //todo create second activity to display event details
+
+    //todo create intent to open location of the event
+
+    //todo create intent to open calendar to schedule event
+
+    //todo add info about the hub somewhere (on logo click?) and indicate it visually
+
+    //todo implement SwipeRefreshLayout
+
+    //todo figure out data storage (firebase? facebook api?)
+
+    //todo implement SharedPreferences to store toggle and notification option
+
+    //todo implement notification permission request (or activity)
+
+    //todo implement notification system
+
 }
