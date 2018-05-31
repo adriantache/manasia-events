7f3c34b0920e8eaf576754ddf1ec9cc55096b8e8
Adrian Tache <contact@adriantache.com>
2018-05-31 16:33:53 +0300

    Implement Picasso to show widget image

--- app/src/main/java/com/adriantache/manasia_events/widget/EventWidget.java
+++ app/src/main/java/com/adriantache/manasia_events/widget/EventWidget.java
@@ -1,17 +1,17 @@
 package com.adriantache.manasia_events.widget;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
 import android.widget.RemoteViews;
 
 import com.adriantache.manasia_events.R;
 import com.adriantache.manasia_events.custom_class.Event;
 import com.adriantache.manasia_events.db.DBUtils;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.adriantache.manasia_events.util.Utils.compareDateToToday;
@@ -35,32 +35,42 @@ public class EventWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.event_widget);
 
+        //fetch the events array from the database
         ArrayList<Event> events = (ArrayList<Event>) DBUtils.readDatabase(context);
-        Event event = null;
 
         //fetch the closest event to today
-        if (events != null) {
+        Event event = null;
+        if (events != null && events.size() != 0) {
             for (Event event1 : events) {
                 if (compareDateToToday(event1.getDate()) > -1)
                     event = event1;
             }
+            //if all events are in the past, just get the most recent one;
+            //should look better than if we just display placeholder text
+            if (event == null) event = events.get(0);
         }
 
-        //if all events are in the past, just get the most recent one;
-        //should look better than if we just display placeholder text
-        if (event == null) event = events.get(0);
-
+        //set the notification text and image
         if (event != null) {
             views.setTextViewText(R.id.title, event.getTitle());
             views.setTextViewText(R.id.date,
-                    extractDayOrMonth(event.getDate(),true)
-                            +"\n"
-                            +extractDayOrMonth(event.getDate(),false));
-            views.setImageViewUri(R.id.thumbnail, Uri.parse(event.getPhotoUrl()));
+                    extractDayOrMonth(event.getDate(), true)
+                            + "\n"
+                            + extractDayOrMonth(event.getDate(), false));
+
+            //fetch the image bitmap with picasso and use it
+            Bitmap bitmap = null;
+
+            try {
+                bitmap = Picasso.get().load(event.getPhotoUrl()).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            views.setImageViewBitmap(R.id.thumbnail, bitmap);
         }
 
-
-        // Construct an Intent object includes web adresss.
+        // Construct an Intent object includes web address.
 //        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://erenutku.com"));
 //        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
 //        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
@@ -69,5 +79,4 @@ public class EventWidget extends AppWidgetProvider {
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
-
 }
