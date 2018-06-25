b49d252e8e3bc44c500e589e10f5a44925c8ab57
Adrian Tache <contact@adriantache.com>
2018-06-25 11:42:31 +0300

    Some tweaks to the EventWidget class

--- app/src/main/java/com/adriantache/manasia_events/widget/EventWidget.java
+++ app/src/main/java/com/adriantache/manasia_events/widget/EventWidget.java
@@ -29,16 +29,16 @@ import static com.adriantache.manasia_events.util.Utils.isEventToday;
  **/
 public class EventWidget extends AppWidgetProvider {
     private static Bitmap bitmap;
-    private Context context;
-    private Event event;
-    private AppWidgetManager appWidgetManager;
-    private int[] appWidgetIds;
+    private static Context context;
+    private static Event event;
+    private static AppWidgetManager appWidgetManager;
+    private static int[] appWidgetIds;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        this.context = context;
-        this.appWidgetManager = appWidgetManager;
-        this.appWidgetIds = appWidgetIds;
+        EventWidget.context = context;
+        EventWidget.appWidgetManager = appWidgetManager;
+        EventWidget.appWidgetIds = appWidgetIds;
 
         //todo fetch remote events before updating widget
 
@@ -57,17 +57,17 @@ public class EventWidget extends AppWidgetProvider {
         ArrayList<Event> events = (ArrayList<Event>) DBUtils.readDatabase(context);
 
         //fetch the closest event to today
-        Event event = null;
-        if (events != null && events.size() != 0) {
+        Event closestEvent = null;
+        if (events != null && !events.isEmpty()) {
             for (Event event1 : events) {
                 if (compareDateToToday(event1.getDate()) > -1)
-                    event = event1;
+                    closestEvent = event1;
             }
-            //if all events are in the past, just get the most recent one;
+            //if all events are in the past, just get the most recent one
             //should look better than if we just display placeholder text
-            if (event == null) event = events.get(0);
+            if (closestEvent == null) closestEvent = events.get(0);
         }
-        return event;
+        return closestEvent;
     }
 
     private void updateWidgetContents(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -101,42 +101,7 @@ public class EventWidget extends AppWidgetProvider {
         }
     }
 
-    private void updateWidgetImages() {
-        for (int appWidgetId : appWidgetIds) {
-            // Construct the RemoteViews object
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.event_widget);
-
-            //set the notification text
-            if (event != null) {
-                if (isEventToday(event.getDate())) {
-                    views.setTextViewText(R.id.title, event.getTitle());
-                    views.setTextViewText(R.id.date, "TODAY");
-                } else {
-                    views.setTextViewText(R.id.title, event.getTitle());
-                    views.setTextViewText(R.id.date,
-                            extractDayOrMonth(event.getDate(), true)
-                                    + "\n"
-                                    + extractDayOrMonth(event.getDate(), false));
-                }
-
-                //set intent to open that event's details
-                Intent intent = new Intent(context, EventDetail.class);
-                intent.putExtra(DBEventIDTag, event.getDatabaseID());
-                PendingIntent pendingIntent = PendingIntent.getActivity(context, 3,
-                        intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                views.setOnClickPendingIntent(R.id.relative_layout, pendingIntent);
-            }
-
-            //set the notification image
-            if (bitmap != null)
-                views.setImageViewBitmap(R.id.thumbnail, bitmap);
-
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
-    }
-
-    private class BitmapAsyncTask extends AsyncTask<String, Void, Void> {
+    private static class BitmapAsyncTask extends AsyncTask<String, Void, Void> {
 
         @Override
         protected Void doInBackground(String... strings) {
@@ -166,6 +131,41 @@ public class EventWidget extends AppWidgetProvider {
         @Override
         protected void onPostExecute(Void aVoid) {
             updateWidgetImages();
+        }
+
+        private void updateWidgetImages() {
+            for (int appWidgetId : appWidgetIds) {
+                // Construct the RemoteViews object
+                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.event_widget);
+
+                //set the notification text
+                if (event != null) {
+                    if (isEventToday(event.getDate())) {
+                        views.setTextViewText(R.id.title, event.getTitle());
+                        views.setTextViewText(R.id.date, "TODAY");
+                    } else {
+                        views.setTextViewText(R.id.title, event.getTitle());
+                        views.setTextViewText(R.id.date,
+                                extractDayOrMonth(event.getDate(), true)
+                                        + "\n"
+                                        + extractDayOrMonth(event.getDate(), false));
+                    }
+
+                    //set intent to open that event's details
+                    Intent intent = new Intent(context, EventDetail.class);
+                    intent.putExtra(DBEventIDTag, event.getDatabaseID());
+                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 3,
+                            intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    views.setOnClickPendingIntent(R.id.relative_layout, pendingIntent);
+                }
+
+                //set the notification image
+                if (bitmap != null)
+                    views.setImageViewBitmap(R.id.thumbnail, bitmap);
+
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            }
         }
     }
 }
