df797b9bebf5b11456e3a7c04a395d746d0ce449
Adrian Tache <contact@adriantache.com>
2018-08-30 10:05:51 +0300

    Try to fix JSON error and improve error hanandling

--- app/src/main/java/com/adriantache/manasia_events/loader/EventLoader.java
+++ app/src/main/java/com/adriantache/manasia_events/loader/EventLoader.java
@@ -5,7 +5,6 @@ import android.support.v4.content.AsyncTaskLoader;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.adriantache.manasia_events.R;
 import com.adriantache.manasia_events.custom_class.Event;
 
 import org.json.JSONArray;
@@ -28,7 +27,7 @@ import static com.adriantache.manasia_events.util.Utils.getImageUrl;
  * Loader to fetch remote JSON file and decode it into a List of Events
  **/
 public class EventLoader extends AsyncTaskLoader<List<Event>> {
-    private static String remoteURL;
+    private String remoteURL;
 
     public EventLoader(Context context, String URL) {
         super(context);
@@ -36,33 +35,34 @@ public class EventLoader extends AsyncTaskLoader<List<Event>> {
     }
 
     private static ArrayList<Event> parseJSON(String JSON) {
-        if (TextUtils.isEmpty(JSON)) return null;
+        if (TextUtils.isEmpty(JSON) || JSON.length() < 50) return new ArrayList<>();
 
         ArrayList<Event> events = new ArrayList<>();
 
         //parse JSON String
         try {
             JSONObject root = new JSONObject(JSON);
-            JSONArray event_title = root.getJSONArray("event_title");
+            JSONArray eventTitle = root.optJSONArray("event_title");
 
-            for (int i = 0; i < event_title.length(); i++) {
-                JSONObject child = event_title.optJSONObject(i);
+            for (int i = 0; i < eventTitle.length(); i++) {
+                JSONObject child = eventTitle.optJSONObject(i);
                 String title = child.optString("name");
-                String date = buildDate(child.getString("month"), child.optString("day"));
+                String date = buildDate(child.optString("month"), child.optString("day"));
                 String description = child.optString("description_long");
                 if (TextUtils.isEmpty(description)) description = child.optString("description");
-                String image_url = child.optString("image_url");
-                if (!TextUtils.isEmpty(image_url)) image_url = getImageUrl(image_url);
+                String imageUrl = child.optString("image_url");
+                if (!TextUtils.isEmpty(imageUrl)) imageUrl = getImageUrl(imageUrl);
 
                 //give the description breathing room
                 if (description != null)
                     description = description.replace("\n", "\n\n");
 
                 if (date != null && title != null && description != null)
-                    events.add(new Event(date, title, description, image_url));
+                    events.add(new Event(date, title, description, imageUrl));
             }
         } catch (JSONException e) {
             Log.e("parseJSON", "Cannot parse JSON", e);
+            events = new ArrayList<>();
         }
 
         return events;
@@ -85,7 +85,7 @@ public class EventLoader extends AsyncTaskLoader<List<Event>> {
     }
 
     //OKHTTP implementation
-    private String getJSON(String url) throws IOException, NullPointerException {
+    private String getJSON(String url) throws IOException {
         //override timeouts to ensure receiving full JSON
         OkHttpClient.Builder b = new OkHttpClient.Builder();
         b.connectTimeout(15, TimeUnit.SECONDS);
