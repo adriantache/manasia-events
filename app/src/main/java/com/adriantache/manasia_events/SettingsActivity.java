82316327838c09bfcffe0f91745dbc858bae7e58
Adrian Tache <contact@adriantache.com>
2018-10-15 17:41:31 +0300

    Remove unneeded listener

--- app/src/main/java/com/adriantache/manasia_events/SettingsActivity.java
+++ app/src/main/java/com/adriantache/manasia_events/SettingsActivity.java
@@ -95,37 +95,13 @@ public class SettingsActivity extends AppCompatActivity {
         devTools.setText(displayText);
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat
-            implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         //in the future, remember to add this line to the styles.xml theme:
         //<item name="preferenceTheme">@style/PreferenceThemeOverlay</item>
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             getPreferenceManager().setStorageDefault();
             addPreferencesFromResource(R.xml.preferences);
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-
-            getPreferenceScreen().getSharedPreferences()
-                    .registerOnSharedPreferenceChangeListener(this);
-        }
-
-        @Override
-        public void onPause() {
-            super.onPause();
-
-            getPreferenceScreen().getSharedPreferences()
-                    .unregisterOnSharedPreferenceChangeListener(this);
-        }
-
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            //todo remove this
-            Toast.makeText(getContext(), "Preference changed: " + key + " - " +
-                    sharedPreferences.getBoolean(key, false), Toast.LENGTH_SHORT).show();
         }
     }
 }
